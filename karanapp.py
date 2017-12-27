1821e9395b8331df5db23ba456b1cafcd62a9bae
karangautam <gautam_karan_singh@hotmail.com>
2017-12-27 19:12:32 +0530

    Add files via upload

---
+++ karanapp.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("English Bot", 
+                     storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
+                     database = mongodb_name,
+                     database_uri = MONGODB_URI)
+
+english_bot.set_trainer(ChatterBotCorpusTrainer)
+english_bot.train("chatterbot.corpus.english")
+#english_bot.train("chatterbot.corpus.english.greetings")
+#english_bot.train("chatterbot.corpus.english.conversations")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
+
+
+if __name__ == "__main__":
+    app.run()
